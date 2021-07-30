f357a405ed64fe9d08630196ed5c5d6e74de5cdf
Nithin6030 <74246964+Nithin6030@users.noreply.github.com>
2021-07-30 14:50:11 -0400

    Initial commit

---
+++ app/src/main/java/com/example/sqliteapplication/ListAdapter.java
@@ -0,0 +1,58 @@
+package com.example.sqliteapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder>{
+
+    private ArrayList<ListData> myData;
+
+
+    public ListAdapter(ArrayList<ListData> myData) {
+        this.myData = myData;
+    }
+    @NonNull
+    @Override
+    public ListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem= layoutInflater.inflate(R.layout.list_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull  ListAdapter.ViewHolder holder, int position) {
+        final ListData myListData = myData.get(position);
+        holder.textView.setText(myData.get(position).getName()+"\n"+myData.get(position).getContact()+"\n"+myData.get(position).getDob());
+        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"click on item: "+myListData.getName(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return myData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+        public RelativeLayout relativeLayout;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.textView = (TextView) itemView.findViewById(R.id.textView);
+            relativeLayout = (RelativeLayout)itemView.findViewById(R.id.relativeLayout);
+        }
+    }
+}
